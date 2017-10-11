7de2da9fa7fb39549fee11d8a2397f276833822f
anonymouse <mr.vadim.kravtsov@mail.ru>
2017-10-11 21:47:59 +0300

    Worked version

--- 1/first.py
+++ 1/first.py
@@ -1,18 +1,50 @@
 from math import exp, sin, pi
+import matplotlib.pyplot as plt
 
-def p(x):
-	return (1+x)/(2-x)
+p = lambda x: (1.0+x)/(2.0-x)
+q =	lambda x: 1.0+exp(-x)
+f = lambda x: 1.0+sin((pi/2.0)*x)
 
-def q(x):
-	return 1+exp(-x)
+alpha = [1.0, 0.0]
+beta = [0.5, 1.0]
+A, B = 1.0, 1.0
 
-def f(x):
-	return 1+sin((pi/2)*x)
 
-def coefficients(N):
-	a, b, c, d = [], [], [], []
-	grid = (float(k)/N for k in xrange(N+1))
-	for x in grid:
-		print x, p(x)
-		
-coefficients(1000)
+def thomas_algorithm(N, a = 0.0, b = 1.0):
+	X = [float(j)/N for j in xrange(N+1)]
+	Y = [0.0 for i in xrange(N+1)]
+	m, k, c, d = [], [], [], []
+	h = (b - a)/N
+	dif = alpha[1]-alpha[0]*h
+	
+	m.append(-2.0 + h*p(a))
+	k.append(1.0 - h*p(a)+(h**2)*q(a))
+	c.append(dif/(m[0]*dif+k[0]*alpha[1]))
+	d.append((k[0]*A*h)/dif+f(a)*(h**2))
+	
+	grid = ((j[0], float(j[1])/N) for j in enumerate(xrange(1, N-1)))
+	
+	for (i, x) in grid:
+		print i,x
+		m.append(-2.0 + h*p(x))
+		k.append(1.0 - h*p(x) + (h**2)*q(x))
+		c.append(1.0/(m[i] - k[i]*c[i-1]))
+		d.append(f(x)*h**2 - k[i]*c[i-1]*d[i-1])
+	
+	Y[N] = (beta[1]*c[N-2]*d[N-2] + B*h)/(beta[1]*(1.0+c[N-2]) + beta[0]*h)
+	
+	for i in xrange(N-1,0,-1):
+		Y[i] = c[i-1]*(d[i-1] - Y[i+1])
+		print Y[i]
+	Y[0] = (alpha[1]*Y[1] - A*h)/dif
+	return X, Y
+
+def plot_result(N):
+	x, y = thomas_algorithm(N)
+	plt.ylabel('y')
+	plt.xlabel('x')
+	plt.grid(True)
+	plt.plot(x, y)
+	plt.show()
+
+plot_result(1200)
