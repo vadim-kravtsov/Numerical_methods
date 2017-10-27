eb8a2027efc1e4fd690fee61d21148aa020b3a23
anonymouse <mr.vadim.kravtsov@mail.ru>
2017-10-27 23:51:48 +0300

    Passed version

--- 1/first.py
+++ 1/first.py
@@ -1,15 +1,17 @@
 from math import log, sin, pi, exp
 import matplotlib.pyplot as plt
 
-p = lambda x: (x + 3.0/2.0)
-q =	lambda x: (x+1.0)/(x+3.0)
-f = lambda x: 4.0*x**3+11.5*x**2+17.0*x+8.5
+p = lambda x: (1.0+x)/(2.0-x)
+q = lambda x: 1.0+exp(-x)
+f = lambda x: 1.0+sin((pi/2.0)*x)
 
 alpha = [1.0, 0.0]
-beta = [0.76, 1.0]
-A, B = 3.0, 16.08
+beta = [0.5, 1.0]
+A, B = 1.0, 1.0
+
 a, b = 0.0, 1.0
 
+N = int(input())
 def thomas_algorithm(N, a = a, b = b, A = A, B = B ):
 	X = [a+float(j)*(b-a)/N for j in xrange(N+1)]
 	Y = [0.0 for i in xrange(N+1)]
@@ -31,7 +33,6 @@ def thomas_algorithm(N, a = a, b = b, A = A, B = B ):
 	u.append(kap[0])
 	v.append(nu[0])
 
-	print u, v
 	grid = ((j[0], a+float(j[1])*(b-a)/N) for j in enumerate(xrange(0, N)))
 	
 	for (i, x) in grid:
@@ -42,7 +43,7 @@ def thomas_algorithm(N, a = a, b = b, A = A, B = B ):
 	for i in xrange(1, N):
 		u.append((m[i])/(k[i] - c[i]*u[i-1]))
 		v.append(((c[i]*v[i-1]-d[i])/(k[i]-c[i]*u[i-1])))
-	
+		print u[i], v[i]
 	Y[N] = (-kap[1]*v[N-1]-nu[1])/(kap[1]*u[N-1]-1.0)
 	
 	for i in xrange(N-1, 0 ,-1):
@@ -50,7 +51,7 @@ def thomas_algorithm(N, a = a, b = b, A = A, B = B ):
 
 	Y[0] = u[0]*Y[1] + v[0]
 
-	for i in xrange(N):
+	for i in xrange(N+1):
 		print '%12e %12e'%(X[i],Y[i])
 	return X, Y
 
@@ -64,7 +65,7 @@ def plot_result(N, a = a, b = b, A = A, B = B):
 	x, y = thomas_algorithm(N, a, b, A, B)
 	plt.plot(x, y)
 	
-plot_result(10)
+plot_result(N)
 plt.savefig('result.png')
 plt.show()
 
