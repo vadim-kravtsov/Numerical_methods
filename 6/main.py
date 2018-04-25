a231755f0322541705a118d84308fc24bed3329c
Vadim Kravtsov <mr.vadim.kravtsov@mail.ru>
2018-04-25 14:52:25 +0300

    Fix some bug

--- 6/main.py
+++ 6/main.py
@@ -1,6 +1,9 @@
 import numpy as np
 from math import sqrt, sin, pi, cos, log
 import matplotlib.pyplot as plt
+import pylab
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
 def phi(x):
 	return 1.0/(1+x**2)**2
@@ -8,12 +11,19 @@ def phi(x):
 def heat_equation_solve():
 	pass
 
+def plot_surface(x, t, U):
+	fig = pylab.figure()
+	ax = Axes3D(fig)
+	x, t = np.meshgrid(x, t)
+	ax.plot_surface(x, t, U,cmap = cm.jet)
+	pylab.show()
+
 def main():
 	a, b = [0,1]
 	#numb of x
 	n = 50 				
 	#numb of layers
-	m = 100 	
+	m = 5000 	
 	h = (b - a)/float(n)
 	tau = h**2/3
 	sigma = tau/h**2
@@ -27,8 +37,10 @@ def main():
 			U[k][i] = (1-tau-2*sigma)*U[k-1][i]+sigma*U[k-1][i+1]+sigma*U[k-1][i-1]
 		U[k][0] = (4*U[k][1]-U[k][2])/3.0
 		U[k][n] = (4*U[k][n-1]-U[k][n-2])/3.0
-		plt.plot(x, U[k])
-	plt.show()
+	plot_surface(x, t, U)
 	print(U)
+
+
+
 
 main()
